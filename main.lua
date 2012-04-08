dd039469636678a40d1b71ebf7438133a55b70b5
Matthew L. McAlpin <matthew.mcalpin@gmail.com>
2012-04-08 17:26:28 -0400

    Centered shaker image.
    Got the shaker image into the center of the phone. Still needs to be cleaned
    up, and perhaps made larger.

---
+++ main.lua
@@ -0,0 +1,4 @@
+local shakerImage = display.newImage("cocktail-shaker.jpg")
+shakerImage:setReferencePoint(display.CenterReferencePoint)
+shakerImage.x = display.contentWidth * 0.5
+shakerImage.y = display.contentHeight * 0.5
