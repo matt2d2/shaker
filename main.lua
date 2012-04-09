37a0a6c26ad7507026e96e4b54498960c892c234
Matthew L. McAlpin <matthew.mcalpin@gmail.com>
2012-04-09 00:47:25 -0400

    Got sound working on start.
    Modified wav file to mp3, so that it would work as a sound file and got it
    to work on start-up. Need it to work when shaking, not start-up.

--- main.lua
+++ main.lua
@@ -2,3 +2,6 @@ local shakerImage = display.newImage("cocktail-shaker.jpg")
 shakerImage:setReferencePoint(display.CenterReferencePoint)
 shakerImage.x = display.contentWidth * 0.5
 shakerImage.y = display.contentHeight * 0.5
+
+local shakerSound = audio.loadSound("shakerSound.mp3")
+shakerSound = audio.play( shakerSound )
