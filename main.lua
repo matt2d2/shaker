1638ed988a6c3af3e5fe3274ee79f77871c517d6
Matthew L. McAlpin <matthew.mcalpin@gmail.com>
2012-04-10 05:37:44 -0400

    Shakes and makes sound.
    Still need to edit the sound, but when you shake the phone, it does make a
    sound.

--- main.lua
+++ main.lua
@@ -1,7 +1,15 @@
+display.setStatusBar(display.HiddenStatusBar)
+
 local shakerImage = display.newImage("cocktail-shaker.jpg")
 shakerImage:setReferencePoint(display.CenterReferencePoint)
 shakerImage.x = display.contentWidth * 0.5
 shakerImage.y = display.contentHeight * 0.5
 
 local shakerSound = audio.loadSound("shakerSound.mp3")
-shakerSound = audio.play( shakerSound )
+local shakeSensor = {}
+function shakeSensor:accelerometer(e)
+	if(e.isShake == true) then
+	shakerSound = audio.play( shakerSound )
+end
+end
+Runtime:addEventListener("accelerometer", shakeSensor)
