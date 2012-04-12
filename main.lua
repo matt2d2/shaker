08bcfca3e1269abdd3bacd0c16e5764889c49a6a
Matthew L. McAlpin <matthew.mcalpin@gmail.com>
2012-04-12 19:40:33 -0400

    Removed debug code, sounds work on shakes.
    Removed the debug code and changed the code so it makes the shaker noise
    every time the phone is shaked.

--- main.lua
+++ main.lua
@@ -8,26 +8,9 @@ shakerImage.y = display.contentHeight * 0.5
 local shakerSound = audio.loadSound("shakerSound.mp3")
 local shakeSensor = {}
 
-local rect = display.newRect( 
-	display.contentWidth * 0.5, display.contentHeight * 0.5,
-	150, 150 )
-
-local rcolor = 0
-local function changeRect()
-	rect:setFillColor( rcolor, rcolor, rcolor );
-	if( rcolor == 255 ) then 
-		rcolor = 0 
-	else
-		rcolor = 255
-	end
-end
-
-
 function shakeSensor:accelerometer(e)
 	if(e.isShake == true) then
-		print( "shake it" )
-		changeRect()
-		shakerSound = audio.play( shakerSound )
+		audio.play( shakerSound )
 	end
 end
 
