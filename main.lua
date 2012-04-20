6f2b6d37483d17abb1bcdcae682f7909c68f4ca3
Matthew L. McAlpin <matthew.mcalpin@gmail.com>
2012-04-20 09:27:44 -0400

    Got content scaling per device of image.
    Needed to load the image as display.newImageRect, not as display.newImage.
    Then pass the width and height of the screen defined in config.lua. The
    image is not yet scaled properly--distorted--looks a little wide.

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 display.setStatusBar(display.HiddenStatusBar)
 
-local shakerImage = display.newImage("shaker.png", true)
+local shakerImage = display.newImageRect("shaker.png", 320, 480)
 shakerImage:setReferencePoint(display.CenterReferencePoint)
 shakerImage.x = display.contentWidth * 0.5
 shakerImage.y = display.contentHeight * 0.5
